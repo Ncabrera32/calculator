ef1a9c0674fecdad810d9dc985fda274a9712162
Ncabrera32 <nathanielcabrera88@yahoo.com>
2022-08-16 20:22:48 -0600

    added button functionality to calculator that prints class and value in console with my explaination on how it works.

--- script.js
+++ script.js
@@ -13,8 +13,43 @@ function updateDisplay(){
 const display = document.querySelector('.calculator-screen');
 display.value = calculator.displayValue
 }
-
 updateDisplay();
+
+//defining calculator-keys as keys with document querySelector attached
+//i want keys to have an event listener for a click and then have an event happen
+//with the => syntax we are telling the code we want run a function.... but I still could write a function.
+//so we create a const of target to be the event we want to executed.
+//to extract these events i need to put target in {} to use destructuring to pass the object { target } as a function
+//after reading about arrow functions you need to use "return", {braces}, and (parentheses)
+//then we can start our if checks
+//1st if check is that if target does not match(selector) then return nothing
+//have to use matches() because target is set as an element in the HTML as "keys" and thats the event we are listening for
+//for the rest of the checks the target event is checking the calculator-keys for the correct class
+//then asking to print in console the class and value listed in the button html
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+  if (!target.matches('button')) {
+    return;
+  }
+  
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
 
 function add(num1, num2){
     let result = num1 + num2
