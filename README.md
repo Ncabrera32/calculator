8a8743c0d7203e2c970480c807bad973c98febc1
Ncabrera32 <nathanielcabrera88@yahoo.com>
2022-09-04 15:30:02 -0600

    got the reset calculator button working, fixed decimal bug, and cleaned up event listners.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ this is the calculator I have been working on for a bit. i built it off git to s
 2nd commit just added a display value to my calculator with explaination on how it works.
 
 3rd commit added button functionality to calculator that prints class and value in console with my explaination on how it works.
+
+4th commit got the keys working.
+
+5th commit got the reset calculator button working, fixed decimal bug, and cleaned up event listners.
--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-//this creates my calculator object to help keep track of values
 const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-  };
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
-  function inputDigit(digit) {
-    const { displayValue, waitingForSecondOperand } = calculator;
-  
-    if (waitingForSecondOperand === true) {
-      calculator.displayValue = digit;
-      calculator.waitingForSecondOperand = false;
-    } else {
-      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    }
-  
-    console.log(calculator);
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
+}
 
 function inputDecimal(dot) {
-  // If the `displayValue` property does not contain a decimal point
+  if (calculator.waitingForSecondOperand === true) {
+  	calculator.displayValue = "0."
+    calculator.waitingForSecondOperand = false;
+    return
+  }
+
   if (!calculator.displayValue.includes(dot)) {
-    // Append the decimal point
     calculator.displayValue += dot;
   }
 }
@@ -30,6 +31,12 @@ function inputDecimal(dot) {
 function handleOperator(nextOperator) {
   const { firstOperand, displayValue, operator } = calculator
   const inputValue = parseFloat(displayValue);
+  
+  if (operator && calculator.waitingForSecondOperand)  {
+    calculator.operator = nextOperator;
+    return;
+  }
+
 
   if (firstOperand == null && !isNaN(inputValue)) {
     calculator.firstOperand = inputValue;
@@ -42,7 +49,6 @@ function handleOperator(nextOperator) {
 
   calculator.waitingForSecondOperand = true;
   calculator.operator = nextOperator;
-  console.log(calculator);
 }
 
 function calculate(firstOperand, secondOperand, operator) {
@@ -59,49 +65,47 @@ function calculate(firstOperand, secondOperand, operator) {
   return secondOperand;
 }
 
-// to update the display I need to make a function in that funciton I need to name my const and is "display" 
-// calculator-screen is selecting my display for the calculator
-// calculator.displayValue pulls 0 from my calculator object 
-// adding .value to the end of my const of display, inputs the 0 from calculator.displayValue into the input field for my calculator screen  
-function updateDisplay(){
-const display = document.querySelector('.calculator-screen');
-display.value = calculator.displayValue
+function resetCalculator() {
+  calculator.displayValue = '0';
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
 }
+
+function updateDisplay() {
+  const display = document.querySelector('.calculator-screen');
+  display.value = calculator.displayValue;
+}
+
 updateDisplay();
 
-//defining calculator-keys as keys with document querySelector attached
-//i want keys to have an event listener for a click and then have an event happen
-//with the => syntax we are telling the code we want run a function.... but I still could write a function.
-//so we create a const of target to be the event we want to executed.
-//to extract these events i need to put target in {} to use destructuring to pass the object { target } as a function
-//after reading about arrow functions you need to use "return", {braces}, and (parentheses)
-//then we can start our if checks
-//1st if check is that if target does not match(selector) then return nothing
-//have to use matches() because target is set as an element in the HTML as "keys" and thats the event we are listening for
-//for the rest of the checks the target event is checking the calculator-keys for the correct class
-//then asking to print in console the class and value listed in the button html
 const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
+keys.addEventListener('click', event => {
   const { target } = event;
+  const { value } = target;
   if (!target.matches('button')) {
     return;
   }
 
-  if (target.classList.contains('operator')) {
-    handleOperator(target.value);
-    updateDisplay();
+  switch (value) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '=':
+      handleOperator(value);
+      break;
+    case '.':
+      inputDecimal(value);
+      break;
+    case 'all-clear':
+      resetCalculator();
+      break;
+    default:
+      if (Number.isInteger(parseFloat(value))) {
+        inputDigit(value);
+      }
   }
 
-  if (target.classList.contains('decimal')) {
-    inputDecimal(target.value);
-		updateDisplay();
-  }
-
-  if (target.classList.contains('all-clear')) {
-    console.log('clear', target.value);
-    return;
-  }
-
-  inputDigit(target.value);
   updateDisplay();
 });
